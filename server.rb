2b531d29d0ba6e3301f9a5a4df7db5d975cb1558
Robert <robert.bahmann@gmail.com>
2013-03-17 18:58:05 +0100

    unless statement korrigiert

--- server.rb
+++ server.rb
@@ -13,14 +13,18 @@ end
 get '/*/set/:uri' do
 	spotify_uri = params[:uri]
 	uri = URI("http://ws.spotify.com/lookup/1/.json?uri=#{spotify_uri}")
+	out = "Not a valid song-uri :("
 
-	song = JSON.parse(Net::HTTP.get(uri))
-	out = "#{song["track"]["name"]} by "
-	song["track"]["artists"].each do |artist|
-		out = "#{out} #{artist["name"]}"
-	end
-
-
-  	current_song = Song.new(spotify_uri)
-  	"New song is set to: #{out}"
+	#fetch song-details 
+	response = Net::HTTP.get(uri)
+	unless response == ""
+		song = JSON.parse(response)
+		out = "#{song["track"]["name"]} by "
+		song["track"]["artists"].each do |artist|
+			out = "#{out} #{artist["name"]}"
+		end
+		current_song = Song.new(spotify_uri)
+  		out = "New song is set to: #{out} :-)"
+  	end
+  	out
 end
