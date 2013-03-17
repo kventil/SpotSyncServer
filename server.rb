0366e2bb420d799cf015120c9b7a4b8fce69b3d8
Robert <robert.bahmann@gmail.com>
2013-03-17 22:43:05 +0100

    Postform auf Head, Http/SpotifyUris werden gleich behandelt

--- server.rb
+++ server.rb
@@ -5,13 +5,13 @@ require './song.rb'
 
 current_song = Song.new
 
-get '/*/get' do 
-	current_song.to_json
-end
+
+def setSong(spotify_uri)
+	if spotify_uri.include? "http"
+		spotify_uri['http://open.spotify.com/track/'] = "spotify:track:"
+	end
 
 
-get '/*/set/:uri' do
-	spotify_uri = params[:uri]
 	uri = URI("http://ws.spotify.com/lookup/1/.json?uri=#{spotify_uri}")
 	out = "Not a valid song-uri :("
 
@@ -23,8 +23,40 @@ get '/*/set/:uri' do
 		song["track"]["artists"].each do |artist|
 			out = "#{out} #{artist["name"]}"
 		end
+		out = "#{out} (#{song["track"]["length"]})"
 		current_song = Song.new(spotify_uri)
   		out = "New song is set to: #{out} :-)"
   	end
   	out
+ end
+
+
+ def submit_form
+ 	'<form name="input" action="/form/set" method="post">
+Song: <input type="text" name="uri">
+<input type="submit" value="Submit">
+</form>'
+ end
+
+
+get '/*/get' do 
+	current_song.to_json
+end
+
+#direct uri-call
+get '/*/set/:uri' do
+	spotify_uri = params[:uri]
+	setSong(spotify_uri)
+end
+
+
+#interface-call
+get '/' do
+ submit_form
+end
+
+#interfacepost
+post '/form/set' do
+	spotify_uri = params[:uri]
+	"#{setSong(spotify_uri)} <br> #{submit_form}"
 end
