3d41631a717e371c250b59677940441e10bb2c1e
Robert <robert.bahmann@gmail.com>
2013-03-24 23:28:14 +0100

    Added correct environment configs for redis-instance

--- server.rb
+++ server.rb
@@ -8,9 +8,11 @@ set :environment, :production
 
 #todo http://localhost:8808/doc_root/sinatra-1.4.1/Sinatra/Helpers.html
 #status + meldungen korrekt implementieren
+#redis://redistogo:3193dc6b3ab4d856efbbf83fe7e12061@tarpon.redistogo.com:9083/
 
-$db = Redis.new
 
+uri = URI.parse(ENV["REDISTOGO_URL"])
+$db = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 def set_song(params)
   $db.set(params[0],params[1])
@@ -36,4 +38,3 @@ get '/' do
   # return static startpage
   File.read(File.join('public', 'index.html'))
 end
-
