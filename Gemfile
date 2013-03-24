5b5b89bbe35d5be87a5676256cbf47667d99231c
Robert <robert.bahmann@gmail.com>
2013-03-24 21:41:46 +0100

    Added redis backend

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source :'rubygems'
 gem 'sinatra', '1.4.1'
+gem 'redis'
--- server.rb
+++ server.rb
@@ -1,74 +1,39 @@
 require 'sinatra'
 require 'net/http'
+require 'redis'
+
 require './song.rb'
 
 set :environment, :production
 
+#todo http://localhost:8808/doc_root/sinatra-1.4.1/Sinatra/Helpers.html
+#status + meldungen korrekt implementieren
 
-$current_song = Song.new
+$db = Redis.new
 
 
-def setSong(spotify_uri)
-  if spotify_uri.include? "http"
-    spotify_uri['http://open.spotify.com/track/'] = "spotify:track:"
-  end
-
-  uri = URI("http://ws.spotify.com/lookup/1/.json?uri=#{spotify_uri}")
-  out = 403
-
-  #fetch song-details
-  response = Net::HTTP.get(uri)
-  unless response == ""
-    song_info = JSON.parse(response)
-    $current_song = Song.new(spotify_uri,song_info)
-    out = 200
-  end
-  out
+def set_song(params)
+  $db.set(params[0],params[1])
 end
 
 
+def get_song(params)
+  $db.get(params[0])
+end
+
 get '/*/get' do
-  $current_song.to_json
+  get_song(params[:splat])
 end
 
 #direct uri-call - returns 200 on sucess and 403 on error
-get '/*/set/:uri' do
-  setSong(params[:uri])
+get '/*/set/*' do
+  set_song(params[:splat])
 end
 
 
-#interface-call
+#servercall
 get '/' do
   # return static startpage
   File.read(File.join('public', 'index.html'))
 end
 
-#Wrapper fuers webinterface
-post '/form/set' do
-  out = "Not a valid Song. Sorry :-("
-  unless setSong(params[:uri]) == 403
-    out = "Partying now to: #{$current_song.to_s}"
-  end
-  out
-end
-
-#returns the current playing song
-#nothing if the time betweet set is > than set+songlength
-get '*/current_song' do
-  now = Time.now.to_i
-  if $current_song.set + $current_song.length > now
-    "#{$current_song.to_s} - #{now - $current_song.set} / #{$current_song.length}"
-  else
-    "empty"
-  end
-end
-
-get '*/remaining' do
-  now = Time.now.to_i
-  remaining  = ($current_song.set + $current_song.length) - now
-  unless 0 > remaining
-    "#{remaining.to_i}"
-  else
-    "0"
-  end
-end
